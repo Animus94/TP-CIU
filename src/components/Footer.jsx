8dd51b038525589404e620e3fc89a6d7fca71df0
EzequielPelozo <ezequiel.pelozo@gmail.com>
2024-07-20 00:33:19 -0300

    Footer agregado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../styles/footer.css"; // Acá se importan los estilos que tiene el navbar en la pagina principal
 
 function Footer() {
-  return <footer>Footer</footer>;
+  return(
+    <footer className="footer">
+      <p>Todos los derechos reservados &copy; 2024 Cafetería El Hurlinghaniano. Dueños: Muruaga Branko, Maricel Benitez, Ezequiel Pelozo, Juan Manuel Britez</p>
+    </footer>
+  ); 
 }
 
 export default Footer;
